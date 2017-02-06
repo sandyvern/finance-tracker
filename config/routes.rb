3ff53a8f962d311c0d4152aa00477997fc31bcc5
Sandy <SVern99@aol.com>
2017-02-06 20:51:17 +0000

    Added friendship model and associations'

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
+  get 'my_friends', to: 'users#my_friends'
   
   resources :user_stocks, except: [:show, :edit, :update]
   
