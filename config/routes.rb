e906874b39b2d97d98e5a514cbd4a5c75cfed2f1
Sandy Hogan <SVern99@aol.com>
2017-02-06 04:03:30 +0000

    Updated views and navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
  
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "user/registrations" }
   resources :user_stocks, except: [:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  # root "welcome#index"
-  root "users#my_portfolio"
+  root "welcome#index"
   
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
